d3e5cb8b4e3e59c33140f8e95480f6c7aa9880ac
wfqdreamcity@163.com <aA6473915>
2016-10-26 11:34:00 +0800

    curl 判断添加错误判断

--- src/lib/curl.go
+++ src/lib/curl.go
@@ -8,14 +8,18 @@ import (
 func HbaseGet(method string,para map[string]string) ([]byte , error){
 	var url string
 	url = hbasehost+method+"?token="+dataToken+"&"
+	var body []byte
 
 	for i ,v := range para {
 		url = url+i+"="+v+"&"
 	}
 
 	resp ,  err := HttpGet(url)
+	if err != nil {
+		return body ,err
+	}
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err = ioutil.ReadAll(resp.Body)
 
 	return body , err
 
